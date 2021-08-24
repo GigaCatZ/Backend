02ef96cd8482c39226ef42d23ecaab8fedb64d83
sudo-lucifer <krittin.niu@student.mahidol.edu>
2021-08-25 02:30:22 +0700

    Implement simple login api (not test yet)

--- src/app.py
+++ src/app.py
@@ -1,14 +1,26 @@
-from flask import Flask
-from flask import request
-from flask_login import LoginManager
+from flask import Flask, session, request
+# from flask_login import LoginManager
 import bcrypt
+
+
+class User:
+    def __init__(self, username, password,email) -> None:
+        self.username = username
+        self.password = password
+        self.email = email
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(username="admin", password=bcrypt.hashpw(b'12345', bcrypt.gensalt(10)), email="admin@muic.com"))
+
 
 app = Flask(__name__)
 app.secret_key = "muicpeeps12345"
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -17,9 +29,14 @@ def login():
     
     if (username != None and password != None):
         passwordToByte = str.encode(password)
-        passwordHash = bcrypt.hashpw(passwordToByte, bcrypt.gensalt(100))
-        # excute to database
-        return True
+
+        searchUser = [i for i in users if (i.username == username)]
+        if (len(searchUser) != 0):
+            if (bcrypt.checkpw(passwordToByte, searchUser[0].password)):
+                session['username'] = username
+                return True
+            else:
+                return False
     return False
 
 
@@ -27,7 +44,6 @@ def login():
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
