c96c2ebf335cf34014b20f69f4ae4d92af1ba78a
sudo-lucifer <krittin.niu@student.mahidol.edu>
2021-08-27 01:36:07 +0700

    Implement whoami api, fix authentication process

--- src/routes.py
+++ src/routes.py
@@ -3,7 +3,6 @@ from flask import current_app as app
 from flask import request, jsonify
 from flask_login import LoginManager, login_user, logout_user, current_user
 import bcrypt
-from flask_login.utils import login_required
 from sqlalchemy.exc import IntegrityError
 
 from .query import read_queries, write_queries
@@ -13,41 +12,34 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    print("pass")
     return read_queries.get_user_from_id(user_id)
 
-# THIS IS A DUMMY THING COPIED FROM AJ KANAT
 
 
-@app.route('/login', methods=['GET'])
-def get_login():
-    return '''
-               <form action='api/login' method='POST'>
-                <input type='text' name='username' id='username' placeholder='username'/>
-                <input type='password' name='password' id='password' placeholder='password'/>
-                <input type='submit' name='submit'/>
-               </form>
-            '''
+@app.route('/api/whoami', methods=['GET'])
+def authenticate():
+    if (current_user.is_authenticated):
+        return jsonify(isLoggedit=True, display_name=current_user.display_name, sky_username=current_user.sky_username)
+    else:
+        return jsonify(isLoggedit=False, display_name="", sky_username="")
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    username = request.form.get('sky_ussername')
+    username = request.form.get('sky_username')
     password = request.form.get('password')
     
+    if (current_user.is_authenticated):
+        return jsonify(username=current_user.sky_username, status=True, message="You already log in")
+
     if (username != None and password != None):
         passwordToByte = password.encode('utf8')
 
         encrypted_password = read_queries.get_encrypted_password(username)
-        # print(encrypted_password)
-        # print(type(encrypted_password))
-        # print(type(bcrypt.hashpw(passwordToByte, bcrypt.gensalt(10))))
 
         if (encrypted_password != None):
-            # only uncomment when testing (in case we manually add password without encrypt lol)
-            # encrypted_password = bcrypt.hashpw(str.encode(encrypted_password), bcrypt.gensalt(10))
             if (bcrypt.checkpw(passwordToByte, encrypted_password.encode('utf8'))):
-                current_user = read_queries.get_user_from_username(username)
-                login_user(current_user, remember=True)
+                cur_user = read_queries.get_user_from_username(username)
+                login_user(cur_user, remember=True)
                 return jsonify(username=username, status=True, message="Login successfully")
     return jsonify(username="", status=False, message="Can not login")
 
@@ -68,13 +60,12 @@ def register():
 
 @app.route('/api/logout', methods=['GET','POST'])
 def logout():
-    user = None
-    try:
-        user = current_user.username
-    except (AttributeError):
+    if(current_user.is_authenticated):
+        user = current_user.sky_username
+        logout_user()
+        return jsonify(status=True, username=user, message="Logout successfully")
+    else:
         return jsonify(status=False, username="", message="User hasnt logged in yet")
-    logout_user()
-    return jsonify(status=True, username=user, message="Logout successfully")
 
 @app.route('/api/test', methods=['GET'])
 def test():
@@ -128,8 +119,4 @@ def display_thread(thread_id: int):
     thread = Thread.query.filter(Thread.id == thread_id)
     return jsonify(title=thread.question, body=thread.body, user=thread.user_id, date_asked=thread.timestamp)
 
-
-# @app.route('/api/test', methods=['GET'])
-# def test():
-    # return "Login as " + str(current_user)
 
