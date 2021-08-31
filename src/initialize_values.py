6242e12400359f09ae70294625dfb1c9a629ad95
nawat <67263644+justnawat@users.noreply.github.com>
2021-08-31 20:31:06 +0700

    initializes with one user, one thread, and two comments

--- src/initialize_values.py
+++ src/initialize_values.py
@@ -1,5 +1,6 @@
-from .models import (Thread, Comment, Tag, db)
+from .models import (Thread, Comment, Tag, Users, db)
 from sqlalchemy import (event, exc)
+from datetime import datetime
 
 
 @event.listens_for(Tag.__table__, 'after_create')
@@ -16,37 +17,24 @@ def create_tags(*args, **kwargs):
     db.session.commit()
 
 
+@event.listens_for(Users.__table__, "after_create")
+def create_fake_users(*args, **kwargs):
+    try:
+        db.session.add(Users(sky_username="u6380496", display_name="Noah", encrypted_password="monkerules", mod=False))
+    finally:
+        db.session.commit()
+
+
 # Just to test out my functions. Don't mind these
-@event.listens_for(Thread.__table__, 'after_create')
-def create_threads(*args, **kwargs):
-    print("I hate my life")
-    thread_user = [num for num in range(6380490, 6380500)]
-    questions = [f"This is question #{n}" for n in range(10)]
-    timestamps = ["2021-09-09 02:30:00" for _ in range(10)]
-    duplicates = [1, 1, 1, 1, 1, 6, 5, 4, 3, 2]
-    likeys = [0 for _ in range(10)]
-    for user, title, time, dupe, like in zip(thread_user, questions, timestamps, duplicates, likeys):
-        try:
-            db.session.add(Thread(user_id=user, question=title, timestamp=time, dupes=dupe, likes=like))
-        except exc.IntegrityError:
-            print("error")
-            continue
+@event.listens_for(Thread.__table__, "after_create")
+def create_fake_threads(*args, **kwargs):
+    db.session.add(Thread(user_id=1, question="This is a question #1", body="This is a body", timestamp=datetime.now(), dupes=5))
+    db.session.add(Thread(user_id=1, question="This is a question #2", body="This is a body", timestamp=datetime.now(), dupes=3))
     db.session.commit()
 
 
 @event.listens_for(Comment.__table__, "after_create")
 def create_fake_comments(*args, **kwargs):
-    print("I hate my life the sequel")
-    users = [num for num in range(6380490, 6380500)]
-    thread_ids = [1 for _ in range(10)]
-    num_likes = [i for i in range(10)]
-    parents = [1 for _ in range(10)]
-    timestamps = ["2021-09-09 02:30:00" for _ in range(10)]
-    bodies = [f"Answer #{num}" for num in range(10)]
-    for user, tid, nlike, parent, time, bodi in zip(users, thread_ids, num_likes, parents, timestamps, bodies):
-        try:
-            db.session.add(Comment(user_id=user, thread_id=tid, likes=nlike, parent_id=parent, timestamp=time, body=bodi))
-        except exc.IntegrityError:
-            print("error")
-            continue
+    db.session.add(Comment(user_id=1, thread_id=1, likes=3, parent_id=1, timestamp=datetime.now(), body="This is an answer #1"))
+    db.session.add(Comment(user_id=1, thread_id=1, likes=5, parent_id=1, timestamp=datetime.now(), body="This is an answer #2"))
     db.session.commit()
