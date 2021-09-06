d54f20eda83b27f26e4c02d0c460e576d431be5f
suhavni <suhavni@gmail.com>
2021-09-07 03:40:24 +0700

    fixed search bug when author have upper case

--- src/routes/pages_api.py
+++ src/routes/pages_api.py
@@ -18,7 +18,7 @@ def search():
             search_title in thread['title'].lower()
 
     def filter_by_display_name(thread, search_display_name):
-        return thread['display_name'] == search_display_name
+        return thread['display_name'].lower() == search_display_name
 
     search_input = request.form.get('search_input')
     type_search = request.form.get('type_search')
